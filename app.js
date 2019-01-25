d37449744adcf530ab8f605d19f9c103d76fd167
Steffen Andre Hagen <steffen.hagen@knowit.no>
2019-01-25 09:29:35 +0100

    added mobile support

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ const note3_e = document.getElementById('note3_e');
 const note4_f = document.getElementById('note4_f');
 const note5_g = document.getElementById('note5_g');
 const note6_a = document.getElementById('note6_a');
+let mobilekey = "";
+
+document.getElementById("c-note-button").addEventListener("click", function () {
+    mobilekey = "a";
+    selector(mobilekey);
+});
+document.getElementById("d-note-button").addEventListener("click", function() {
+    mobilekey = "s";
+    selector(mobilekey);
+});
+document.getElementById("e-note-button").addEventListener("click", function() {
+    mobilekey = "d";
+    selector(mobilekey);
+});
+document.getElementById("f-note-button").addEventListener("click", function() {
+    mobilekey = "f";
+    selector(mobilekey);
+});
+document.getElementById("g-note-button").addEventListener("click", function() {
+    mobilekey = "j";
+    selector(mobilekey);
+});
+document.getElementById("a-note-button").addEventListener("click", function() {
+    mobilekey = "k";
+    selector(mobilekey);
+});
+
+
 
 // Init lottie animation.
 const bongoCat = lottie.loadAnimation({
@@ -23,37 +51,10 @@ const bongoCat = lottie.loadAnimation({
 bongoCat.addEventListener('data_ready', function () {
     //Listen for keyboard input (a,s,d,f,j,k = valid inputs).
    window.addEventListener('keypress', function (e) {
-       bongoCat.setSpeed(.25);
-       switch(e.key){
-           case "a":
-               bongoCat.playSegments([1,3]);
-               playNote(note1_c);
-               break;
-           case "s":
-               bongoCat.playSegments([3,5]);
-               playNote(note2_d);
-               break;
-           case "d":
-               bongoCat.playSegments([5,7]);
-               playNote(note3_e);
-               break;
-           case "f":
-               bongoCat.playSegments([7,9]);
-               playNote(note4_f);
-               break;
-           case "j":
-               bongoCat.playSegments([9,11]);
-               playNote(note5_g);
-               break;
-           case "k":
-               bongoCat.playSegments([11,13]);
-               playNote(note6_a);
-               break;
-           default:
-               console.log("invalid key");
-               break;
-       }
-   });
+       selector(e.key);
+
+    });
+
 });
 
 // Helper function to play notes.
@@ -63,4 +64,37 @@ function playNote(note) {
         note.currentTime = 0;
     }
     note.play();
+}
+
+function selector (input) {
+    bongoCat.setSpeed(.25);
+    switch(input){
+        case "a":
+            bongoCat.playSegments([1,3]);
+            playNote(note1_c);
+            break;
+        case "s":
+            bongoCat.playSegments([3,5]);
+            playNote(note2_d);
+            break;
+        case "d":
+            bongoCat.playSegments([5,7]);
+            playNote(note3_e);
+            break;
+        case "f":
+            bongoCat.playSegments([7,9]);
+            playNote(note4_f);
+            break;
+        case "j":
+            bongoCat.playSegments([9,11]);
+            playNote(note5_g);
+            break;
+        case "k":
+            bongoCat.playSegments([11,13]);
+            playNote(note6_a);
+            break;
+        default:
+            console.log("invalid key");
+            break;
+    }
 }
